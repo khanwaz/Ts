f7c7896ad690bedeccdf24b34fc3c5a150ee6b2c
Akram khan <khanwaz@gmail.com>
2021-10-06 18:15:28 +0530

    latest_upadates

---
+++ index.js
@@ -0,0 +1,18 @@
+var age = 12;
+age = 2;
+var fullName = 'akram';
+fullName = 'Aleena';
+var isFullName = true;
+isFullName = false;
+var team;
+team = ['Akram', 'Aleena'];
+var personDetail;
+personDetail = {
+    dob: 24101988,
+    location: 'Nellore'
+};
+console.log('Age:', age);
+console.log('Full Name:', fullName);
+console.log('Is Full Name:', isFullName);
+console.log('Person Details:', personDetail);
+console.log('Team :', team);
--- index.ts
+++ index.ts
@@ -1,2 +1,28 @@
-let age : number = 12;
-console.log ("age");
+let age: number = 12;
+age = 2;
+
+let fullName: string = 'akram';
+fullName = 'Aleena';
+
+let isFullName: boolean = true;
+isFullName = false;
+
+let team : string[];
+team = ['Akram','Aleena']
+
+let personDetail : {
+    dob: number;
+    location: string;
+};
+
+personDetail = {
+    dob: 24101988,
+    location: 'Nellore',
+};
+
+console.log('Age:',age);
+console.log('Full Name:',fullName);
+console.log('Is Full Name:',isFullName);
+console.log('Person Details:',personDetail);
+console.log('Team :',team);
+
