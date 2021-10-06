80080d61376c38ae76b616cf882df2df380a4277
Akram khan <khanwaz@gmail.com>
2021-10-06 19:08:16 +0530

    Till union types

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var age = 12;
 age = 2;
-var fullName = 'akram';
-fullName = 'Aleena';
+var fullName = "akram";
+fullName = "Aleena";
 var isFullName = true;
 isFullName = false;
 var team;
-team = ['Akram', 'Aleena'];
+team = ["Akram", "Aleena"];
 var personDetail;
 personDetail = {
     dob: 24101988,
-    location: 'Nellore'
+    location: "Nellore"
 };
-console.log('Age:', age);
-console.log('Full Name:', fullName);
-console.log('Is Full Name:', isFullName);
-console.log('Person Details:', personDetail);
-console.log('Team :', team);
+var person;
+person = "Akram";
+console.log("Person :", person);
+person = 33;
+var newPerson;
+newPerson = ['ak', 'ak', 'fk'];
+console.log("New Person :", newPerson);
+newPerson = ['akram khan', ['1', 'string'], personDetail];
+console.log("Age:", age);
+console.log("Full Name:", fullName);
+console.log("Is Full Name:", isFullName);
+console.log("Person Details:", personDetail);
+console.log("Team :", team);
+console.log("Person :", person);
+console.log("New Person :", newPerson);
