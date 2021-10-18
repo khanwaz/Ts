5590ca63fe60df3e26ae315c5ddf1a860e4872df
Akram khan <khanwaz@gmail.com>
2021-10-18 11:38:50 +0530

    updated till classes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
 var age = 12;
 age = 2;
 var fullName = "akram";
@@ -16,9 +21,43 @@ person = "Akram";
 console.log("Person :", person);
 person = 33;
 var newPerson;
-newPerson = ['ak', 'ak', 'fk'];
+newPerson = ["ak", "ak", "fk"];
 console.log("New Person :", newPerson);
-newPerson = ['akram khan', ['1', 'string'], personDetail];
+newPerson = ["akram khan"];
+var publicSchool;
+var privateSchool;
+privateSchool = {
+    schoolName: "D.T.E.A",
+    schoolCode: 0861
+};
+publicSchool = {
+    schoolName: "I.R.C",
+    schoolCode: 862
+};
+//Function and types
+function add(a, b) {
+    return a + b;
+}
+//Generic
+function insertAtStart(array, value) {
+    var resultArray = __spreadArray([value], array);
+    return resultArray;
+}
+var numArray = insertAtStart([1, 2, 3], 5);
+var strArray = insertAtStart(['s', 'd', 'f'], 'a');
+//Classes and Typescript
+var Student = /** @class */ (function () {
+    function Student(first, last, age, courses) {
+        this.firstName = first;
+        this.lastName = last;
+        this.age = age;
+        this.courses = courses;
+    }
+    return Student;
+}());
+var student = new Student('AK', 'KH', 33, ['ang']);
+console.log('NumArray :', numArray);
+console.log('StrArray :', strArray);
 console.log("Age:", age);
 console.log("Full Name:", fullName);
 console.log("Is Full Name:", isFullName);
@@ -26,3 +65,7 @@ console.log("Person Details:", personDetail);
 console.log("Team :", team);
 console.log("Person :", person);
 console.log("New Person :", newPerson);
+console.log("Private School :", privateSchool);
+console.log("Public School :", publicSchool);
+console.log("Addtion :", add(2, 3));
+console.log("Classes Student :", student);
--- index.ts
+++ index.ts
@@ -31,8 +31,61 @@ let newPerson: string | string[];
 newPerson = ["ak", "ak", "fk"];
 console.log("New Person :", newPerson);
 
-newPerson = ["akram khan", ["1", "string"], personDetail];
+newPerson = ["akram khan"]
 
+//Alias
+type School = {
+  schoolName : string;
+  schoolCode : number;
+};
+
+let publicSchool : School;
+let privateSchool : School;
+
+privateSchool = {
+  schoolName : "D.T.E.A",
+  schoolCode : 0861
+};
+
+publicSchool = {
+  schoolName : "I.R.C",
+  schoolCode : 862
+};
+
+//Function and types
+function add (a: number, b: number){
+  return a + b;
+}
+
+//Generic
+function insertAtStart <I> (array : I[], value:I)
+{
+  const resultArray = [value, ...array];
+  return resultArray;
+}
+
+const numArray = insertAtStart([1,2,3], 5);
+const strArray = insertAtStart(['s','d','f'],'a');
+
+//Classes and Typescript
+class Student {
+  firstName : string;
+  lastName : string;
+  age : number;
+  courses : string[];
+
+  constructor(first: string,last: string,age:number,courses: string[]){
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.courses = courses;
+  }
+}
+
+const student = new Student('AK','KH',33,['ang']);
+
+console.log('NumArray :', numArray);
+console.log('StrArray :', strArray);
 console.log("Age:", age);
 console.log("Full Name:", fullName);
 console.log("Is Full Name:", isFullName);
@@ -40,3 +93,9 @@ console.log("Person Details:", personDetail);
 console.log("Team :", team);
 console.log("Person :", person);
 console.log("New Person :", newPerson);
+console.log("Private School :", privateSchool);
+console.log("Public School :", publicSchool);
+console.log("Addtion :", add(2,3));
+console.log("Classes Student :", student);
+
+
